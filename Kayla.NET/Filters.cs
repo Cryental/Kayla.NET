a7cae7e24e4c7bb5054b80bfbcb21f71a8597ac7
Cryental <Cryental@DESKTOP-7A1SULB>
2020-09-23 22:41:25 +0600

    Added Add/Remove Subtitle Sync Filter

--- Kayla.NET/Filters.cs
+++ Kayla.NET/Filters.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using Kayla.NET.Models;
 
@@ -20,6 +21,46 @@ namespace Kayla.NET
             }
 
             return filteredItems;
+        }
+
+        public static List<SubtitleItem> IncreaseSyncTime(double seconds, List<SubtitleItem> data)
+        {
+            var fixedItems = new List<SubtitleItem>();
+
+            var convertedSeconds = TimeSpan.FromSeconds(seconds);
+
+            foreach (var f in data)
+            {
+                var newStartTime = new TimeSpan(f.StartTime * 10000).Add(convertedSeconds).Ticks / 10000;
+                var newEndTime = new TimeSpan(f.EndTime * 10000).Add(convertedSeconds).Ticks / 10000;
+
+                f.StartTime = newStartTime;
+                f.EndTime = newEndTime;
+
+                fixedItems.Add(f);
+            }
+
+            return fixedItems;
+        }
+
+        public static List<SubtitleItem> DecreaseSyncTime(double seconds, List<SubtitleItem> data)
+        {
+            var fixedItems = new List<SubtitleItem>();
+
+            var convertedSeconds = TimeSpan.FromSeconds(seconds);
+
+            foreach (var f in data)
+            {
+                var newStartTime = new TimeSpan(f.StartTime * 10000).Subtract(convertedSeconds).Ticks / 10000;
+                var newEndTime = new TimeSpan(f.EndTime * 10000).Subtract(convertedSeconds).Ticks / 10000;
+
+                f.StartTime = newStartTime;
+                f.EndTime = newEndTime;
+
+                fixedItems.Add(f);
+            }
+
+            return fixedItems;
         }
     }
 }
