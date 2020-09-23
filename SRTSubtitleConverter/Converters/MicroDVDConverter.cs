9d23bbaf204fd75cf03c2dc49120128bd00504a4
Cryental <Cryental@DESKTOP-7A1SULB>
2020-09-23 16:06:38 +0600

    Added MicroDVDController

---
+++ SRTSubtitleConverter/Converters/MicroDVDConverter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using SRTSubtitleConverter.Models;
+
+namespace SRTSubtitleConverter.Converters
+{
+    public class MicroDVDConverter : ISubtitleConverter
+    {
+        public readonly float DefaultFrameRate = 23.976f;
+
+        public string Convert(List<SubtitleItem> data)
+        {
+            var filteredItems = new List<string>();
+
+            foreach (var d in data)
+            {
+                if (string.IsNullOrEmpty(d.Text))
+                {
+                    continue;
+                }
+
+                var startTime = Math.Round(d.StartTime * DefaultFrameRate / 1000);
+                var endTime = Math.Round(d.EndTime * DefaultFrameRate / 1000);
+
+                var result = Regex.Replace(d.Text, "(\r\n|\r|\n)", @"|");
+                result = result.Replace(Environment.NewLine, "|");
+
+                filteredItems.Add($"{{{startTime}}}{{{endTime}}}{result}");
+            }
+
+            return string.Join(Environment.NewLine, filteredItems);
+        }
+    }
+}
